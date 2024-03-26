4274513032d78992194dd48a267b4ad27ce7487d
Divyashree <107257824+shreebharathi@users.noreply.github.com>
2024-03-26 23:43:01 +0530

    Add files via upload

---
+++ temp/login.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<!-- Coding By CodingNepal - www.codingnepalweb.com -->
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>AlumNexus Login | Signup</title>
+    <link rel="stylesheet" href="login.css">
+  </head>
+  <body>
+    <div class="container flex">
+      <div class="facebook-page flex">
+        <div class="text">
+          <img src="logo.jpeg" width="300p" height="300p">
+          <p>Signup to</p>
+          <p> Forge bonds with your college community</p>
+        </div>
+        <form action="#">
+          <input type="email" placeholder="Email or phone number" required>
+          <input type="password" placeholder="Password" required>
+          <div class="link">
+            <button type="submit" class="login">Login</button>
+            <a href="#" class="forgot">Forgot password?</a>
+          </div>
+          <hr>
+          <div class="button">
+            <a href="#">Sign Up</a>
+          </div>
+        </form>
+      </div>
+    </div>
+  </body>
+</html>
